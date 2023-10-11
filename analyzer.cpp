593a3c78073e1c7e42ef821bf1bb35692fc73e92
Jonathan Wang <cobaltsleet49@gmail.com>
2023-10-11 10:53:02 -0400

    edit

--- analyzer.cpp
+++ analyzer.cpp
@@ -103,30 +103,30 @@ int main() {
 
     cout << "Linear Search for 'mzzzz'" << "\n";
     const long start3 = systemTimeNanoseconds();
-    int index3 = linear_search(stringData, "not_here");
+    int index3 = linear_search(stringData, "mzzzz");
     const long end3 = systemTimeNanoseconds();
     cout << end3 - start3 << "\n";
     cout << index3 << "\n";
 
     cout << "Binary Search for 'mzzzz'" << "\n";
     const long start4 = systemTimeNanoseconds();
-    int index4 = binary_search(stringData, "not_here");
+    int index4 = binary_search(stringData, "mzzzz");
     const long end4 = systemTimeNanoseconds();
     cout << end4 - start4 << "\n";
     cout << index4 << "\n";
 
     cout << "Linear Search for 'aaaaa'" << "\n";
     const long start5 = systemTimeNanoseconds();
-    int index5 = linear_search(stringData, "not_here");
+    int index5 = linear_search(stringData, "aaaaa");
     const long end5 = systemTimeNanoseconds();
     cout << end5 - start5 << "\n";
     cout << index5 << "\n";
 
     cout << "Binary Search for 'aaaaa'" << "\n";
     const long start6 = systemTimeNanoseconds();
-    int index6 = binary_search(stringData, "not_here");
+    int index6 = binary_search(stringData, "aaaaa");
     const long end6 = systemTimeNanoseconds();
     cout << end6 - start6 << "\n";
-    cout << index2 << "\n";
+    cout << index6 << "\n";
 
 }
